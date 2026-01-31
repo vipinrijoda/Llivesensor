2f891bd3e4c45c94afcb751a6892b7ddafc3affb
vipin yadav <vipinyadavrijoda9131@gmail.com>
2026-01-31 22:23:46 +0530

    after editing List

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import find_packages,setup
 from typing import List
 
 
-def get_requirements()->list[str]:
-    requirements_list=list[str]=[]
+def get_requirements()->List[str]:
+    requirements_list=List[str]=[]
 
     return requirements_list
 
